1994429954ea83e10bc68992413f9aa9928fa14d
Abhishek kr <abhimait1909@gmail.com>
2022-08-16 00:53:25 +0530

    fix problems with application spec
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- cypress/integration/e2e/application_spec.js
+++ cypress/integration/e2e/application_spec.js
@@ -11,14 +11,14 @@ describe("Application Spec", () => {
     cy.wait("@extensionFileLoad", { timeout: 20000 });
   })
 
-  it("Visit MeshMap Application", () => {
+  it("Render MeshMap Application", () => {
     cy.get('[data-cy="application-drawer"]').click();
     cy.contains("Applications")
     cy.get("#MUIDataTableBodyRow-applications-0").should("be.visible").click(); //convention: MUIDataTableBodyRow + type  + rowIndex
     cy.wait(2000);
     cy.get("body").then(body => {
         if (body.find("[aria-describedby='notistack-snackbar'] #notistack-snackbar").length > 0) {
-          cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Unable to render")
+          cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Error")
         }
       })
   });
@@ -26,25 +26,24 @@ describe("Application Spec", () => {
   it("Rename and Saving Application", () => {
     cy.get("#component-drawer-Application").should('be.visible')
     cy.get("#design-name-textfield").type("Changed Application Name with cypress");
-    cy.intercept('/api/application').as('applicationSave')
+    cy.intercept('/api/application*').as('applicationSave')
  
     // TODO - Saving Application request intercept need to be fixed.
 
-    // cy.wait("@applicationSave").then(() => {
-    //     // move to drawer and check for update
-    //     cy.get("[data-cy='design-drawer']").click();
-    //     cy.wait(5000); // wait for seconds, because the subscritions cannot be tracked for now
-    //     cy.get("#MUIDataTableBodyRow-applications-0 p").contains("Changed Application Name with cypress");
-    //   })
+    cy.wait("@applicationSave").then(() => {
+        // move to drawer and check for update
+        cy.get("[data-cy='design-drawer']").click();
+        cy.wait(5000); // wait for seconds, because the subscritions cannot be tracked for now
+        cy.get("#MUIDataTableBodyRow-applications-0 p").contains("Changed Application Name with cypress");
+      })
   })
 
   it("Search an Application", () => {
+    const applicationName = "emoji.yaml" // sample name of an application
     cy.get("[data-cy='application-drawer']").click();
-    cy.get('[data-test-id="Search"]').type("Changed Application Name with cypress");
-    cy.intercept("/api/application*").as("applicationDelete")
-    cy.wait(3000)
-    // TODO - Application was not saved so not able to Search
-    // cy.get("#MUIDataTableBodyRow-applications-0").should("be.visible").contains("Changed Application Name with cypress");
+    cy.get('[data-test-id="Search"]').type(applicationName);
+    cy.intercept("/api/application*").as("applicationSearch")
+    cy.wait("@applicationSearch")
+    cy.get("#MUIDataTableBodyRow-applications-0").should("be.visible").contains(applicationName);
   })
-
 })
