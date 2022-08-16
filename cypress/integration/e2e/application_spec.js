cfeebc46a98abebec81f5619335d96185b4b3e45
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-08-16 16:29:43 +0530

    add deployment for application
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- cypress/integration/e2e/application_spec.js
+++ cypress/integration/e2e/application_spec.js
@@ -1,4 +1,5 @@
 import {DESIGNER} from "../../support/constants"
+import '@4tw/cypress-drag-drop'
 describe("Application Spec", () => {
   beforeEach(() => {
     cy.login();
@@ -24,7 +25,7 @@ describe("Application Spec", () => {
   });
 
   it("Rename and Saving Application", () => {
-    cy.get("#component-drawer-Application").should('be.visible')
+    cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container")
     cy.get("#design-name-textfield").type("Changed Application Name with cypress");
     cy.intercept('/api/application*').as('applicationSave')
  
@@ -46,4 +47,41 @@ describe("Application Spec", () => {
     cy.wait("@applicationSearch")
     cy.get("#MUIDataTableBodyRow-applications-0").should("be.visible").contains(applicationName);
   })
+
+  it("Deploy an Application", () => {
+    const applicationName = "emoji.yaml" // sample name of an application
+    cy.get("[data-cy='application-drawer']").click();
+    cy.get('[data-test-id="Search"]').type(applicationName);
+    cy.intercept("/api/application*").as("patternPost")
+    cy.wait("@patternPost")
+    cy.get("#MUIDataTableBodyRow-applications-0").should("be.visible").contains(applicationName).click();
+    cy.wait(2000);
+    cy.get("body").then(body => {
+      if (body.find("[aria-describedby='notistack-snackbar'] #notistack-snackbar").length > 0) {
+        cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Unable to render")
+      }
+    })
+    cy.get("#deploy-design-btn").click();
+    // modal opens
+    cy.intercept("/api/pattern/deploy*").as("applicationDeploy")
+    cy.get('[data-cy="deploy-btn-confirm"]').click();
+    cy.wait("@applicationDeploy").then(() => {
+      // cy.get("[data-cy='progress-snackbar']").contains("Deploying design");
+      cy.get("body").then(body => {
+        if (body.find("[aria-describedby='notistack-snackbar'] #notistack-snackbar").length > 0) {
+          cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Error")
+        }
+      })
+    })
+  });
+
+  it("Drag All Visible component on canvas", () => {
+    cy.get(".component-drawer-svg-container[draggable='true']").each(ele => {
+      const elem = cy.get(ele);
+      elem.click();
+      elem.drag("#cy-canvas-container")
+      cy.wait(500)
+    })
+  });
+
 })
