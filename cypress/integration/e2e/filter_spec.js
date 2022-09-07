7e2965e14af4ec63594b37e93a1d2d49a9a32bb2
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-09-08 01:09:48 +0530

    add sample filter for test
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- cypress/integration/e2e/filter_spec.js
+++ cypress/integration/e2e/filter_spec.js
@@ -13,6 +13,8 @@ describe("Filter Spec", () => {
         cy.wait("@extensionFileLoad", { timeout: 20000 });
       })
 
+      const filterName = "Basic Auth for Istio" // sample name of an application
+
       it("Render Filters", () => {
         cy.get("[data-cy='filter-drawer']").click();
         cy.get("#MUIDataTableBodyRow-filters-0").should("be.visible").click(); //convention: MUIDataTableBodyRow + type  + rowIndex
@@ -27,22 +29,22 @@ describe("Filter Spec", () => {
 
       it("Rename Filter", () => {
         cy.get("#component-drawer-Application").should('be.visible') // .drag("#cy-canvas-container") Filters drag and drop on canvas is disabled for now.
-        cy.get("#design-name-textfield").type("Changed Filter Name with cypress");
+        cy.get("#design-name-textfield").type(filterName);
         cy.intercept('/api/filter').as('filterSave')
         cy.wait("@filterSave").then(() => {
           // move to drawer and check for update
           cy.get("[data-cy='filter-drawer']").click();
           cy.wait(5000); // wait for seconds, because the subscritions cannot be tracked for now
-          cy.get("#MUIDataTableBodyRow-filters-0 p").contains("Changed Filter Name with cypress");
+          cy.get("#MUIDataTableBodyRow-filters-0 p").contains(filterName);
         })
       })
 
       it("Search a filter", () => {
         cy.get("[data-cy='filter-drawer']").click();
-        cy.get('[data-test-id="Search"]').type("Changed Filter Name with cypress");
+        cy.get('[data-test-id="Search"]').type(filterName);
         cy.intercept("/api/filter*").as("filterSearch")
         cy.wait("@filterSearch")
-        cy.get("#MUIDataTableBodyRow-filters-0").should("be.visible").contains("Changed Filter Name with cypress");
+        cy.get("#MUIDataTableBodyRow-filters-0").should("be.visible").contains(filterName);
       });
 
 })
