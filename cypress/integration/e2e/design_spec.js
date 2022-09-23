bd30f5cd72045f25bf087ab05e9acac004dcc837
Abhishek kr <abhimait1909@gmail.com>
2022-09-23 17:13:12 +0530

    fixes: deploy design fails
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- cypress/integration/e2e/design_spec.js
+++ cypress/integration/e2e/design_spec.js
@@ -14,6 +14,8 @@ describe("Designer Spec", () => {
     cy.wait("@extensionFileLoad", { timeout: 20000 });
   })
 
+  const argoRolloutDesign = "no-modify-argo-rollout-application-for-cypress";
+
   it("Load MeshMap Design with a click", () => {
     cy.get("[data-cy='design-drawer']").click();
     cy.get("#MUIDataTableBodyRow-patterns-0", {timeout: 30000});
@@ -29,7 +31,8 @@ describe("Designer Spec", () => {
 
   it("Rename Design", () => {
     cy.wait(1000);
-    cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container")
+    cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container");
+    cy.get("[data-cy='design-drawer']").click(); // to close the rjsf form by click event
     cy.get("#design-name-textfield").type("Changed Name with cypress");
     cy.intercept('/api/pattern').as('patternSave')
     cy.wait("@patternSave").then(() => {
@@ -54,16 +57,21 @@ describe("Designer Spec", () => {
     cy.get("[data-cy='design-drawer']").click();
     cy.get("#MUIDataTableBodyRow-patterns-0", {timeout: 30000})
     cy.get("#MUIDataTableBodyRow-patterns-0").click();
-    cy.get('[data-test-id="Search"]').type("Changed Name with cypress");
+    cy.get('[data-test-id="Search"]').type(argoRolloutDesign);
     cy.intercept("/api/pattern*").as("patternPost")
     cy.wait("@patternPost")
-    cy.get("#MUIDataTableBodyRow-patterns-0").should("be.visible").contains("Changed Name with cypress").click();
+    cy.get("#MUIDataTableBodyRow-patterns-0").should("be.visible").contains(argoRolloutDesign);
+    cy.get("#MUIDataTableBodyRow-patterns-0").click();
+    cy.wait("@patternPost");
     cy.wait(2000);
+
+    // rendering done up until this point
     cy.get("body").then(body => {
       if (body.find("[aria-describedby='notistack-snackbar'] #notistack-snackbar").length > 0) {
         cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Unable to render")
       }
     })
+
     cy.get("#deploy-design-btn").click();
     // modal opens
     cy.intercept("/api/pattern/deploy*").as("patternDeploy")
@@ -72,7 +80,7 @@ describe("Designer Spec", () => {
       // cy.get("[data-cy='progress-snackbar']").contains("Deploying design");
       cy.get("body").then(body => {
         if (body.find("[aria-describedby='notistack-snackbar'] #notistack-snackbar").length > 0) {
-          cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Error")
+          cy.get("[aria-describedby='notistack-snackbar'] #notistack-snackbar").should("not.contain", "Failed")
         }
       })
     })
