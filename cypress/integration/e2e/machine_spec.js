b04249da2bd3061cc4bc771da2d748b6cfe2dab4
Abhishek kr <abhimait1909@gmail.com>
2022-10-11 22:34:57 +0530

    state machine tests changes
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- cypress/integration/e2e/machine_spec.js
+++ cypress/integration/e2e/machine_spec.js
@@ -11,16 +11,17 @@ describe("State Machine Spec", () => {
         cy.visit("/extension/meshmap");
         cy.intercept("/api/provider/extension*").as("extensionFileLoad");
         cy.wait("@extensionFileLoad", { timeout: 20000 });
+        cy.intercept('/api/pattern').as('patternSave')
     })
 
     context("State Machine Test", () => {
         it("DO NOT allow merge of a design into itself", () => {
             cy.get('[data-cy="design-drawer"]').click();
-            cy.get("#MUIDataTableBodyRow-patterns-0", { timeout: 30000 }).should("be.visible"); 
+            cy.get("#MUIDataTableBodyRow-patterns-0", { timeout: 30000 }).should("be.visible");
 
             // First design dropped on canvas
             cy.get("#MUIDataTableBodyRow-patterns-0").click();
-            
+
             // Drop same design on canvas to test (do not merge) 
             cy.get("#MUIDataTableBodyRow-patterns-0").drag("#cy-canvas-container", { force: true });
             cy.wait(2000);
@@ -32,38 +33,27 @@ describe("State Machine Spec", () => {
         });
 
         it('DO save design every time a node is added or removed', () => {
-        // Save on Additon of node  
+            // Save on Additon of node  
             // A node is added on canvas    
-            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", {force: true});
-            // Go to that design
-            cy.get("[data-cy='design-drawer']").click();
-            cy.get("#MUIDataTableBodyRow-patterns-0").click();
-            // Save the design
-            cy.intercept('/api/pattern').as('patternSave')
+            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", { force: true });
             cy.wait("@patternSave");
-        // Save on deletion of node
-            cy.get('[data-cy="component-drawer"]').click()
-            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", {force: true});
+            // Save on deletion of node
+            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", { force: true });
             // Perform delete action
             cy.get("#component-delete").click();
-            // Save the design
-            cy.intercept('/api/pattern').as('patternSave')
+            // Check Saving of the design
             cy.wait("@patternSave");
-          });
+        });
 
         it("DO save designs when all nodes are emptied on user request (reset canvas or all node deletes)", () => {
             // Drop a node on canvas
             cy.get('[data-cy="component-drawer"]').click()
-            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", {force: true});
-            // Go to that design
-            cy.get("[data-cy='design-drawer']").click();
-            cy.get("#MUIDataTableBodyRow-patterns-0").click();
+            cy.get("#component-drawer-Application").should('be.visible').drag("#cy-canvas-container", { force: true });
             // Reset canvas by clear/delete all nodes
             cy.get('[data-cy="reset-btn"]').click();
-            // Save the design
-            cy.intercept('/api/pattern').as('patternSave')
+            // Check Saving the design
             cy.wait("@patternSave");
-          });
+        });
     })
 
 });
