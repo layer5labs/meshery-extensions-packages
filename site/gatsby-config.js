8680c1e328e9036500094ece84be15e5bcd3854d
Lee Calcote <lee.calcote@layer5.io>
2023-08-07 05:30:18 -0500

    mv back
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

---
+++ site/gatsby-config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Layer5 Recognition Program`,
+    siteUrl: `https://badges.layer5.io`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
