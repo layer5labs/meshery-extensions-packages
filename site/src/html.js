ca16317dac26cff533f2fa62569e1ce446285077
JeremiahObikoya <132661422+JeremiahObikoya@users.noreply.github.com>
2023-10-04 00:41:40 +0100

    Update html.js
    Added the Google Tag Manager Scripts to the head and body.
    
    Signed-off-by: JeremiahObikoya <132661422+JeremiahObikoya@users.noreply.github.com>

--- site/src/html.js
+++ site/src/html.js
@@ -6,6 +6,13 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PDTVCLXM');</script>
+        <!-- End Google Tag Manager -->
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -15,6 +22,10 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDTVCLXM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         {props.preBodyComponents}
         <div
           key={`loader`}
