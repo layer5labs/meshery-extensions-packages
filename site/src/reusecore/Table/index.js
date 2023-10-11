4df2e8ae312734550fc05b776f71ca5d1120135d
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-10-11 12:55:16 +0100

    chore: init of the avatar component

--- site/src/reusecore/Table/index.js
+++ site/src/reusecore/Table/index.js
@@ -7,6 +7,12 @@ export const TableBody = styled.tbody``;
 
 export const TableHeader = styled.thead``;
 
-export const TableRow = styled.tr``;
+export const TableRow = styled.tr`
+  position: relative;
+  border-bottom: 1px solid lightgray;
+  color: dark;
+  background-color: ${props =>
+    Number(props.id) % 2 ? 'primary-100' : 'inherit'};
+`;
 
 export const TH = styled.th``;
