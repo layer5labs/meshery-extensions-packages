abf18260824cdc3a92b5bfb1756da769f0942c98
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-10-11 12:44:09 +0100

    update: of the avatar component

--- site/src/api/client.js
+++ site/src/api/client.js
@@ -2,10 +2,7 @@ import Axios from 'axios';
 
 export const BASE_URL =
   process.env.LEADERBOARD_BASE_URL ||
-  //   'https://crossorigin.me/https://discuss.layer5.io';
   'https://corsproxy.io/?https://discuss.layer5.io';
-
-// "proxy": "https://discuss.layer5.io/"
 export const client = Axios.create({
   baseURL: BASE_URL,
   headers: {
---
+++ site/src/reusecore/Table/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const TD = styled.td``;
+
+export const TableBody = styled.tbody``;
+
+export const TableHeader = styled.thead``;
+
+export const TableRow = styled.tr``;
+
+export const TH = styled.th``;
