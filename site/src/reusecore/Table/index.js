c86bfd1b4c39282449da9a4b3e542d4ef1bf321b
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-10-11 12:58:07 +0100

    chore: update of the td component

--- site/src/reusecore/Table/index.js
+++ site/src/reusecore/Table/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const TD = styled.td``;
+export const TD = styled.td`
+  font-size: 14px;
+  font-weight: normal;
+  text-transform: capitalize;
+  white-space: nowrap;
+  padding: 14px 5px;
+`;
 
 export const TableBody = styled.tbody``;
 
@@ -11,8 +17,7 @@ export const TableRow = styled.tr`
   position: relative;
   border-bottom: 1px solid lightgray;
   color: dark;
-  background-color: ${props =>
-    Number(props.id) % 2 ? 'primary-100' : 'inherit'};
+  background-color: ${props => (Number(props.id) % 2 ? '#E6FBF7' : 'inherit')};
 `;
 
 export const TH = styled.th``;
