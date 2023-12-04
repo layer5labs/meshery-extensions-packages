978d1b7fc9df5fe44e32f1411ce63641123d2d79
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-12-04 16:22:37 +0100

    chore: updates

---
+++ site/webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js or gatsby-node.js
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      // Add more rules for other file types if needed
+    ],
+  },
+};
