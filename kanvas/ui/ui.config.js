8c54ae45e28a27049ed5339a525b2479c08b1a9e
Aabid Sofi <mailtoaabid01@gmail.com>
2024-11-17 01:41:03 +0530

    add custom logos
    Signed-off-by: Aabid Sofi <mailtoaabid01@gmail.com>

--- kanvas/ui/ui.config.js
+++ kanvas/ui/ui.config.js
@@ -1,14 +1,9 @@
-const {
-  AnimatedMesheryDark,
-} = require("./components/LoadingComponents/Animations/AnimatedMesheryCSS");
-const {
-  default: AnimatedMeshery,
-} = require("./components/LoadingComponents/Animations/AnimatedMesheryCSS");
+const { AnimatedLogoDark, AnimatedLogo } = require("./AnimatedLogo");
 
 module.exports = {
   components: {
     navigator: false, // set false to disable the navigator component ( default: true )
   },
-  AnimatedLogo: AnimatedMeshery,
-  AnimatedLogoDark: AnimatedMesheryDark,
+  AnimatedLogo: AnimatedLogo,
+  AnimatedLogoDark: AnimatedLogoDark,
 };
