74ab781d9b3ad736d3e832c79dad80c02c7a40c9
Lee Calcote <lee.calcote@layer5.io>
2025-11-25 12:20:34 -0600

    Delete kanvas directory
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- kanvas/ui/ui.config.js
+++
@@ -1,9 +0,0 @@
-const { AnimatedLogoDark, AnimatedLogo } = require("./AnimatedLogo");
-
-module.exports = {
-  components: {
-    navigator: false, // set false to disable the navigator component ( default: true )
-  },
-  AnimatedLogo: AnimatedLogo,
-  AnimatedLogoDark: AnimatedLogoDark,
-};
